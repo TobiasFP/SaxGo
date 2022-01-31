6890f479b15aaa17d0ab1b1e1dfe820dec43a1c4
Tobias <tobias@osandweb.dk>
2022-01-31 15:52:51 +0100

    Now exporting SellOrder

--- Sax_test.go
+++ Sax_test.go
@@ -108,7 +108,7 @@ func TestSellOrder(t *testing.T) {
 		return
 	}
 
-	stockRes, err := saxo.sellOrder("5001025814")
+	stockRes, err := saxo.SellOrder("5001025814")
 	if err != nil {
 		t.Errorf(err.Error())
 		return
